55eb25644bacaaf947263edc2758b138a2929008
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-18 16:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let ularray = document.getElementsByTagName("li");
+console.log(ularray);
+function findEle(ularray)
+{
+  for(let i = 0 ; i < ularray.length ; i++)
+  {
+   if (ularray[i].id == "level") {
+      alert(`The level of the element is: ${i}`);
+      break;
+   }
+  }
+}
+findEle(ularray);
